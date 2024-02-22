cb3e3d90f506375c1fd56f6386125f799a7fbb4d
yun9699 <cha23215@daum.net>
2024-02-22 11:47:20 +0900

    yun readtop100 테이블출력기능 추가

--- read_top100.js
+++ read_top100.js
@@ -4,16 +4,23 @@ const Input = require ('./input')
 const uri = process.env.DB_ATLAS_URL;
 const client = new MongoClient(uri);
 
+
 async function read_top100() {
   try {
     await client.connect();
     const result = await client.db("butube").collection("MUSIC").find({ music_rank: { $exists: true } }).toArray();
-    const data = [
-      ['순위', '제목', '가수', '테마'],
-      ...result.map((doc) => [doc.music_rank, doc.music_name, doc.music_singer, doc.music_theme])
-    ];
-    const output = table.table(data);
-    console.log(output);
+    const formattedResults = result.map(item => {
+      return {
+        '순위': item.music_rank,
+        '곡명': item.music_name,
+        '가수': item.music_singer,
+        '테마': item.music_theme
+      };
+    });
+
+    // 수정된 결과를 테이블로 출력합니다.
+    console.table(formattedResults);
+    
   } catch (e) {
     console.log(e.message);
   } finally {
