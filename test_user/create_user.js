bbe51b267ff366111c722098562a7c6e9879cded
seyun00 <reny83@naver.com>
2024-02-22 11:58:46 +0900

    complete_login.js_csy

--- test_user/create_user.js
+++ test_user/create_user.js
@@ -18,7 +18,7 @@ async function create_user() {
     console.log("전화번호를 입력해주세요.")
     let userPhone = await Input.uInput()
 
-    let qry = {_id: userID, user_pw: userPW, user_name: userName, user_phone: userPhone};
+    let qry = {user_id: userID, user_pw: userPW, user_name: userName, user_phone: userPhone};
     const result = await client.db("butube").collection("USER").insertOne(qry);
     console.log("회원가입이 완료되었습니다.")
 
---
+++ test_user/login.js
@@ -0,0 +1,36 @@
+const {MongoClient} = require ('mongodb');
+const Input = require ('./input')
+
+const uri = process.env.DB_ATLAS_URL;
+const client = new MongoClient(uri);
+
+async function login() {
+  try{
+    await client.connect();
+    console.log("-------로그인-------")
+    console.log("아이디를 입력해주세요.")
+    let userID = await Input.uInput();
+
+    let sql01 = {user_id: userID}
+    const result01 = await client.db("butube").collection("USER").findOne(sql01);
+    if (result01 === null) {
+      console.log("존재하지 않는 아이디입니다.")
+      process.exit();
+    } else{
+      let password = result01.user_pw
+      console.log("비밀번호를 입력해주세요.")
+      let userPW = await Input.uInput();
+      if (password !== userPW){
+        console.log("잘못된 비밀번호입니다.");
+      }else{
+        console.log("로그인이 완료되었습니다.")
+      }
+    }
+  }catch(e){
+    console.log(e.message);
+  }finally{
+    await client.close();
+    process.exit();
+  }
+}
+login();
