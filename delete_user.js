9293e423729c355cebc0030e5dbe94110b5087a3
seyun00 <reny83@naver.com>
2024-02-22 12:20:15 +0900

    complete_delete_user.js_csy

--- delete_user.js
+++ delete_user.js
@@ -4,27 +4,21 @@ const Input = require ('./input')
 const uri = process.env.DB_ATLAS_URL;
 const client = new MongoClient(uri);
 
-async function login() {
+async function delete_user() {
   try{
     await client.connect();
-    console.log("-------로그인-------")
+    console.log("-------회원 삭제-------")
     console.log("아이디를 입력해주세요.")
     let userID = await Input.uInput();
 
-    let sql01 = {user_id: userID}
-    const result01 = await client.db("butube").collection("USER").findOne(sql01);
+    let qry01 = {user_id: userID}
+    const result01 = await client.db("butube").collection("USER").findOne(qry01);
     if (result01 === null) {
       console.log("존재하지 않는 아이디입니다.")
       process.exit();
     } else{
-      let password = result01.user_pw
-      console.log("비밀번호를 입력해주세요.")
-      let userPW = await Input.uInput();
-      if (password !== userPW){
-        console.log("잘못된 비밀번호입니다.");
-      }else{
-        console.log("로그인이 완료되었습니다.")
-      }
+      const result02 = await client.db("butube").collection("USER").deleteOne(qry01);
+      console.log("회원 삭제가 완료되었습니다.")
     }
   }catch(e){
     console.log(e.message);
@@ -33,4 +27,5 @@ async function login() {
     process.exit();
   }
 }
-login();
+
+delete_user();
